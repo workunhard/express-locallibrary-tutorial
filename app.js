9e7e543eb1abd985e70c70eae2351f8104c6b47a
workunhard <workun.code@gmail.com>
2023-05-28 16:29:19 -0700

    readying for deployment to Railway

--- app.js
+++ app.js
@@ -9,7 +9,29 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -30,6 +52,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression()); // Compress all routes (https://expressjs.com/en/advanced/best-practice-performance.html
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
