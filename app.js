93a9f4b9dc1778298966ea874e202da24d21c22c
workunhard <workun.code@gmail.com>
2023-05-29 01:07:46 -0700

    removed bootstrap; restyled layout

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
+  max: 100,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
